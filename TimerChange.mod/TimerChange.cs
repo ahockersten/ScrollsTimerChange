dc9defae70a3c0aae78544c3b8789fadd9d5fce2
Anders Höckersten <anders@hockersten.se>
2013-07-14 13:25:58 +0200

    Turn ends correctly

--- TimerChange.mod/TimerChange.cs
+++ TimerChange.mod/TimerChange.cs
@@ -11,12 +11,20 @@ using Mono.Cecil;
 
 namespace TimerChange.mod {
     public class TimerChange : BaseMod {
+        private FieldInfo activeColorField;
+        private FieldInfo leftColorField;
         private FieldInfo roundTimeField;
+        private FieldInfo roundTimerField;
         private FieldInfo showClockField;
+        private MethodInfo endTurnMethod;
 
         public TimerChange() {
+            activeColorField = typeof(BattleMode).GetField("activeColor", BindingFlags.Instance | BindingFlags.NonPublic);
+            leftColorField = typeof(BattleMode).GetField("leftColor", BindingFlags.Instance | BindingFlags.NonPublic);
             roundTimeField = typeof(BattleMode).GetField("roundTime", BindingFlags.Instance | BindingFlags.NonPublic);
+            roundTimerField = typeof(BattleMode).GetField("roundTimer", BindingFlags.Instance | BindingFlags.NonPublic);
             showClockField = typeof(BattleMode).GetField("showClock", BindingFlags.Instance | BindingFlags.NonPublic);
+            endTurnMethod = typeof(BattleMode).GetMethod("endTurn", BindingFlags.Instance | BindingFlags.NonPublic);
         }
 
         public static string GetName() {
@@ -31,6 +39,7 @@ namespace TimerChange.mod {
             try {
                 return new MethodDefinition[] {
                     scrollsTypes["BattleMode"].Methods.GetMethod("_handleMessage", new Type[]{typeof(Message)}),
+                    scrollsTypes["BattleMode"].Methods.GetMethod("OnGUI", new Type[]{}),
                 };
             }
             catch {
@@ -47,9 +56,22 @@ namespace TimerChange.mod {
             if (info.target is BattleMode && info.targetMethod.Equals("_handleMessage")) {
                 BattleMode target = (BattleMode)info.target;
                 Message msg = (Message) info.arguments[0];
-                Console.WriteLine("handling messageeru!");
-                showClockField.SetValue(target, true);
-                roundTimeField.SetValue(target, 30);
+                if (msg is GameInfoMessage) {
+                    showClockField.SetValue(target, true);
+                    roundTimeField.SetValue(target, 30);
+                }
+            }
+            if (info.target is BattleMode && info.targetMethod.Equals("OnGUI")) {
+                BattleMode target = (BattleMode)info.target;
+                if (activeColorField.GetValue(target).Equals(leftColorField.GetValue(target))) {
+                    float roundTimer = (float)roundTimerField.GetValue(target);
+                    float roundTime = (float)roundTimeField.GetValue(target);
+                    float timePassed = (roundTimer >= 0f) ? Mathf.Floor(Time.time - roundTimer) : 0f;
+                    int seconds = Mathf.Max(0, (int)(roundTime + 1 - timePassed)); // add +1 so round stops 1 second AFTER hitting 0
+                    if (seconds == 0) {
+                        endTurnMethod.Invoke(target, new object[] {});
+                    }
+                }
             }
         }
     }
